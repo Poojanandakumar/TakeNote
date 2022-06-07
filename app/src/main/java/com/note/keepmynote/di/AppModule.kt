640fece5f11b49d3c07970de215c7e40404f5788
pooja <poojanandakumar34@gmail.com>
2022-06-07 21:01:17 +0530

    create app module for hilt

---
+++ app/src/main/java/com/note/keepmynote/di/AppModule.kt
@@ -0,0 +1,16 @@
+package com.note.keepmynote.di
+
+import android.content.Context
+import androidx.transition.Visibility
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+}
